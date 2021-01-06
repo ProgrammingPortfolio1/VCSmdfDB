b03b37b14797878738675287ec9b5b192ee571bb
ProgrammingPortfolio1 <70398262+ProgrammingPortfolio1@users.noreply.github.com>
2021-01-07 00:36:16 +0200

    Initial commit

---
+++ VCSmdfDB/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VCSmdfDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query = textBox1.Text;
+            String connStr = "Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|library.mdf;" +
+                                "Integrated Security=True;Connect Timeout=30;User Instance=True";
+            SqlConnection conn = new SqlConnection(connStr);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Command Executed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            conn.Close();
+        }
+    }
+}
